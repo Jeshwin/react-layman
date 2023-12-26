b386f7243450044765ba2042028e95ff16907eb1
Jeshwin Prince <jeshwinjprince@gmail.com>
2023-12-25 21:43:07 -0800

    add drop target highlights

--- src/components/window.jsx
+++ src/components/window.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react"
+import { motion } from "framer-motion"
+
 export function Window({
     children,
     id,
@@ -6,6 +9,18 @@ export function Window({
     width = 1,
     height = 1,
 }) {
+    const [hoveredArea, setHoveredArea] = useState(null)
+
+    const handleHover = (area) => {
+        setHoveredArea(area)
+        console.log("Hovered area is " + area)
+    }
+
+    const handleHoverExit = () => {
+        setHoveredArea(null)
+        console.log("Hovered area is null")
+    }
+
     return (
         <div
             id={id}
@@ -19,6 +34,52 @@ export function Window({
                 {title}
             </div>
             <WindowContent>{children}</WindowContent>
+            <motion.div
+                id={`highlighter-${id}`}
+                className={`absolute rounded-lg border-2 border-fuchsia-600`}
+                style={{
+                    background:
+                        "repeating-linear-gradient(45deg, transparent, transparent 16px, #d946ef 16px, #d946ef 32px)",
+                    opacity: 0.2,
+                }}
+                initial={{ top: "-50%", height: "0%", opacity: 0 }} // Initial position off-screen
+                animate={{
+                    opacity: hoveredArea == null ? 0 : 0.2,
+                    top: hoveredArea === "bottom" ? "auto" : 0,
+                    bottom: hoveredArea === "top" ? "auto" : 0,
+                    left: hoveredArea === "right" ? "auto" : 0,
+                    right: hoveredArea === "left" ? "auto" : 0,
+                    width:
+                        hoveredArea === "left" || hoveredArea === "right"
+                            ? "33.33%"
+                            : "auto",
+                    height:
+                        hoveredArea === "top" || hoveredArea === "bottom"
+                            ? "33.33%"
+                            : "auto",
+                }}
+                transition={{ duration: 0.1 }}
+            ></motion.div>
+            <div
+                onMouseEnter={() => handleHover("top")}
+                onMouseLeave={handleHoverExit}
+                className="absolute top-0 left-0 right-0 h-1/3 bg-transparent"
+            ></div>
+            <div
+                onMouseEnter={() => handleHover("right")}
+                onMouseLeave={handleHoverExit}
+                className="absolute top-0 bottom-0 right-0 w-1/3 bg-transparent"
+            ></div>
+            <div
+                onMouseEnter={() => handleHover("bottom")}
+                onMouseLeave={handleHoverExit}
+                className="absolute left-0 bottom-0 right-0 h-1/3 bg-transparent"
+            ></div>
+            <div
+                onMouseEnter={() => handleHover("left")}
+                onMouseLeave={handleHoverExit}
+                className="absolute top-0 left-0 bottom-0 w-1/3 bg-transparent"
+            ></div>
         </div>
     )
 }
