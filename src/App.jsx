da672ed4a7f37e1df8df717d562dc958073cc957
Jeshwin Prince <jeshwinjprince@gmail.com>
2024-04-04 15:48:20 -0700

    restart!

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-import WindowManager from "./components/windowmanager"
-import initialLayout from "./initialLayout.json"
-import Basics from "./components/basics"
-import DragNDrop from "./components/dragndrop/dragndrop"
-import { DndProvider } from "react-dnd"
-import { HTML5Backend } from "react-dnd-html5-backend"
+import WindowManager from "./windowmanager";
 
 export default function App() {
     return (
-        <div className="bg-slate-900">
-            <div className="container py-16 mx-auto text-slate-50">
-                <WindowManager initialLayout={initialLayout} />
+        <div className="bg-zinc-950">
+            <div className="container py-16 mx-auto text-white">
+                <WindowManager />
             </div>
-            <div className="container py-16 mx-auto text-slate-50">
-                <Basics />
-            </div>
-            <DndProvider backend={HTML5Backend}>
-                <div className="container py-16 mx-auto text-slate-50">
-                    <DragNDrop />
-                </div>
-            </DndProvider>
         </div>
-    )
+    );
 }
---
+++ src/windowmanager.jsx
@@ -0,0 +1,71 @@
+import {nanoid} from "nanoid";
+
+export default function WindowManager() {
+    // Variables for handlebar dimensions
+    const handleBarThickness = 8;
+    return (
+        <div className="h-[calc(100vh-128px)] relative">
+            <div
+                id={nanoid()}
+                style={{
+                    inset: "0% 50% 0% 0%",
+                    position: "absolute",
+                    margin: `${handleBarThickness / 2}px`,
+                }}
+                className="p-2 rounded-lg bg-zinc-800"
+            >
+                <div className="w-full h-full grid place-content-center text-center text-xl">
+                    {nanoid()}
+                </div>
+            </div>
+            <div
+                id={nanoid()}
+                style={{
+                    inset: "0% 0% 0% 50%",
+                    position: "absolute",
+                    marginLeft: `-${handleBarThickness / 2}px`,
+                }}
+                className="w-2 z-10 grid place-content-center hover:cursor-ew-resize"
+            >
+                <div className="w-0.5 h-10 rounded-full bg-zinc-50"></div>
+            </div>
+            <div
+                id={nanoid()}
+                style={{
+                    inset: "0% 0% 50% 50%",
+                    position: "absolute",
+                    margin: `${handleBarThickness / 2}px`,
+                }}
+                className="p-2 rounded-lg bg-zinc-800"
+            >
+                <div className="w-full h-full grid place-content-center text-center text-xl">
+                    {nanoid()}
+                </div>
+            </div>
+            <div
+                id={nanoid()}
+                style={{
+                    inset: "50% 0% 50% 50%",
+                    position: "absolute",
+                    marginTop: `-${handleBarThickness / 2}px`,
+                }}
+                className="h-2 z-10 grid place-content-center hover:cursor-ns-resize"
+            >
+                <div className="h-0.5 w-10 rounded-full bg-zinc-50"></div>
+            </div>
+            <div
+                id={nanoid()}
+                style={{
+                    inset: "50% 0% 0% 50%",
+                    position: "absolute",
+                    margin: `${handleBarThickness / 2}px`,
+                }}
+                className="p-2 rounded-lg bg-zinc-800"
+            >
+                <div className="w-full h-full grid place-content-center text-center text-xl">
+                    {nanoid()}
+                </div>
+            </div>
+        </div>
+    );
+}
